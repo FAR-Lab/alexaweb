f313c8be017b13cd116d6a73fbd574d4e68c3b91
ilan mandel <ilan.mandel1@gmail.com>
2020-06-25 00:14:27 -0400

    updated creds

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="farlabalexaweb"
+Security_Profile_ID  = "amzn1.application.3b61b4b812fa4b79b0a127e5cf5b0129"
+Client_ID = "amzn1.application-oa2-client.353fa8dc4e40420fbe3ecf903ab3774f"
+Client_Secret = "35f59acbf65b2ffc600571bde5514a95926d9b256697346fd0eda7d1584d501f"
+Product_ID = "A15H159A9RUTXL"
 
 #Redis
 redis_url = ""
